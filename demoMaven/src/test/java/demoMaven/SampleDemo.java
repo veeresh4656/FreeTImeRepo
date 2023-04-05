c4e72a3455f91c632acba2fa7a5cbc5a53a1b752
USER <USER@DESKTOP-FUGEFD6>
2023-04-05 15:16:30 +0530

    commit

---
+++ demoMaven/src/test/java/demoMaven/SampleDemo.java
@@ -0,0 +1,14 @@
+package demoMaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleDemo {
+@Test
+public void test_01() {
+System.out.println("Welcome to Maven"); 
+}
+	
+	
+	
+}
